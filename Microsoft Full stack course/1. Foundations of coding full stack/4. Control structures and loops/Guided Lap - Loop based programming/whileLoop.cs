431ad5b69c5fec4101f4a89e4b6d0680c75ea1ed
milushev91 <mmilushev91@gmail.com>
2026-03-15 15:09:36 +0200

    Create whileLoop.cs

---
+++ Microsoft Full stack course/1. Foundations of coding full stack/4. Control structures and loops/Guided Lap - Loop based programming/whileLoop.cs
@@ -0,0 +1,17 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int counter = 1;
+		
+		while (counter <= 10) 
+		
+		{
+			Console.WriteLine(counter);
+			counter++;
+		}		
+		
+	}
+}
