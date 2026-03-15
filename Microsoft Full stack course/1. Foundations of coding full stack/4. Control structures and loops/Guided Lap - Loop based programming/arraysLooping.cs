bee58c2852376eb63ea63840e1a3eae1fdab03a9
milushev91 <mmilushev91@gmail.com>
2026-03-15 15:06:37 +0200

    Create arraysLooping.cs

---
+++ Microsoft Full stack course/1. Foundations of coding full stack/4. Control structures and loops/Guided Lap - Loop based programming/arraysLooping.cs
@@ -0,0 +1,15 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+		int[] numbers = {1, 2, 3, 4, 5};
+		
+		for (int i = 0; i < numbers.Length; i++) 
+		{
+			Console.WriteLine(numbers[i]);
+		}
+		
+	}
+}
