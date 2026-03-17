e67c4103392e633a34afda167f050806ba81c736
milushev91 <mmilushev91@gmail.com>
2026-03-17 10:35:17 +0200

    Create displayWelcomeMessage.cs

---
+++ Microsoft Full stack course/1. Foundations of coding full stack/5. Functions, methods and scope/Guided Lab/displayWelcomeMessage.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace HelloWorld
+{
+	public class Program
+	{
+	  static void DisplayWelcomeMessage() 
+	  
+	  {
+	    Console.WriteLine("Welcome to our program!");
+	  }
+		public static void Main(string[] args)
+		{
+			DisplayWelcomeMessage();
+		}
+	}
+}
