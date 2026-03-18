b0fcc7749e039eabab9f2260158c8d64786062b9
milushev91 <mmilushev91@gmail.com>
2026-03-18 17:32:21 +0200

    Create activity.cs

---
+++ Microsoft Full stack course/1. Foundations of coding full stack/activity.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace HelloWorld
+{
+	public class Program
+	{
+	  public static bool CanAddTask(string task1, task2, task3) 
+	  
+	  {
+	    if (!task1 || !task2 || !task3)
+	    
+	    {
+	      return true;
+	    }
+	    
+	    return falase;
+	  }
+		public static void Main(string[] args)
+		{
+		  // intialize variable for tasks and their status
+			string task1;
+			string task2;
+			string task3;
+			bool task1Completed = false;
+			bool task2Completed = false;
+			bool task3Completed = false;
+			
+			//keep program active
+			while (true) 
+			
+			{
+			  //prompt user for new task
+			  string newTask = Console.ReadLine();
+			  
+			  
+			}
+			
+		}
+	}
+}
