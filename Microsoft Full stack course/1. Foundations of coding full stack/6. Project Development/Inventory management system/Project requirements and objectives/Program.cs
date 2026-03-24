0467743dc4ee38baf092a66bc810284a0be49803
milushev91 <mmilushev91@gmail.com>
2026-03-24 15:29:30 +0200

    Create Program.cs

---
+++ Microsoft Full stack course/1. Foundations of coding full stack/6. Project Development/Inventory management system/Project requirements and objectives/Program.cs
@@ -0,0 +1,192 @@
+using System;
+using System.Collections.Generic;
+
+
+namespace InventoryManager
+{
+	public class InventoryManager
+	{
+	  public static bool programContinue = true;
+	  public static List<string> products = new List<string>();
+	  public static List<int> quantities = new List<int>();
+	  public static List<double> prices = new List<double>();
+	  
+	  // Main Methods
+	  public static void DisplayActions()
+	  
+	  {
+	    Console.WriteLine("Select action(1, 2, 3 or 4):");
+	    Console.WriteLine("1. Add product.");
+	    Console.WriteLine("2. Update product stock level.");
+	    Console.WriteLine("3. View invetory.");
+	    Console.WriteLine("4. Remove product.");
+	    Console.WriteLine("5. Exit system.");
+	    
+	  }
+	  
+	  //Add new product to inventory
+	  public static void AddProduct()
+	  
+	  {
+	    string productName = GetProductName();
+	    int productQuantity = GetProductQuantity();
+	    
+	    if (productQuantity == -1)
+	    
+	    {
+	      DisplayErrorMessage("quantity");
+	      return;
+	    }
+	    
+	    double productPrice = GetProductPrice();
+
+	    if (productPrice == -1)
+	    
+	    {
+	      DisplayErrorMessage("price");
+	      return;
+	    }
+	    
+	    products.Add(productName);
+	    quantities.Add(productQuantity);
+	    prices.Add(productPrice);
+	    
+	    string action = $"Product {productName} with quantity of {productQuantity} and price of ${productPrice} was added to invetory.";
+	    DisplayConfirmationMessage(action);
+	    
+	  }
+	  
+	  public static void UpdateProductStockLevel()
+	  
+	  {
+	    string productName = GetProductName();
+	    int productIndex = GetProductIndex(productName);
+	    
+	    if (productIndex == -1)
+	      {
+	        DisplayErrorMessage("product name");
+	        return;
+	      }
+      	    
+	  }
+	  
+	  // helper methods
+	  
+
+	  
+	  public static void DisplayErrorMessage(string errorType)
+	  
+	  {
+      Console.WriteLine($"Invalid {errorType}. Try again!");
+	  }
+	  
+	  public static void DisplayConfirmationMessage(string action)
+	  
+	  {
+	    Console.WriteLine($"Success. {action}.");
+	  }
+	  
+	  	  // Prompts user for product name and returns it
+	  public static string GetProductName()
+	  
+	  {
+	    Console.WriteLine("Enter product name:");
+	    string productName = Console.ReadLine();
+	    
+	    return productName;
+	  }
+	  
+	  //Prompts user for product quantity and returns it. 
+	  //If quantity is 0 or less or not an integer. Returns -1.
+	  
+	  public static int GetProductQuantity()
+	  
+	  {
+	    Console.WriteLine("Enter product quantity:");
+	    string input = Console.ReadLine();
+	    int quantity;
+	    
+	    if (int.TryParse(input, out quantity) && quantity > 0)
+	    
+	    {
+	      return quantity;
+	    }
+	    
+	    return -1;
+	    
+	  }
+	  
+	   //Prompts user for product price and returns it. 
+	  //If price is 0 or less or not an integer. Returns -1.
+	  public static double GetProductPrice()
+	  
+	  {
+	    Console.WriteLine("Enter product price:");
+	    string input = Console.ReadLine();
+	    double price;
+	    
+	    if (double.TryParse(input, out price) && price > 0)
+	    
+	    {
+	      return price;
+	    }
+	    
+	    return -1;
+	    
+	  }
+	  
+	  //Return product index if it is inventory or - 1.
+	  public static int GetProductIndex(string productName)
+	  
+	  {
+	    int index;
+	    
+	    for (int i = 0; i < products.Count; i++)
+	    
+	    {
+	      if (products[i] == productName)
+	      
+	      {
+	        return i;
+	      }
+	    }
+	    
+	    return -1;
+	  }
+	  
+		public static void Main(string[] args)
+		{
+			
+			while (programContinue)
+			
+			{
+			  DisplayActions();
+			  string actionNumber = Console.ReadLine();
+
+			  switch (actionNumber)
+			  
+			  {
+			    case "1":
+			      AddProduct();
+			      break;
+			    case "2":
+			      UpdateProductStockLevel();
+			      break;
+			   // case 3:
+			   //   ViewInventory();
+			   //   break;
+			   // case 4:
+			   //   RemoveProduct();
+			   //   break;
+			   // default:
+			   //   Console.WriteLine("Invalid action number. Try again!");
+			   //   break;
+			  }
+			  
+			  break;
+			}
+			
+			
+		}
+	}
+}
