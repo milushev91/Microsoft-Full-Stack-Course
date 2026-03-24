eee740f65ac3c2985ea6dc71d0946fc669cdb963
milushev91 <milushev_1990@abv.bg>
2026-03-24 07:55:44 +0200

    added starting project

---
+++ Microsoft Full stack course/1. Foundations of coding full stack/6. Project Development/Inventory management system/InventoryApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+
+
+class InvetoryManager
+
+{
+    public static bool programContinue = true;
+    public static List<string> products = [];
+    public static List<int> quantities = [];
+    public static List<double> prices = [];
+
+    public static void DisplayMainMenu()
+
+    {
+        Console.WriteLine("Choose option(1, 2, 3 or 4): ");
+        Console.WriteLine("1. Add new product.");
+        Console.WriteLine("2. Update product stock.");
+        Console.WriteLine("3. View Inventory.");
+        Console.WriteLine("4. Remove product.");
+        Console.WriteLine("5. Exit inventory manager.");
+        
+    }
+    static void Main(string[] args)
+
+    {
+        while (programContinue)
+
+        {
+            DisplayMainMenu();
+            string choice = Console.ReadLine();
+
+            switch (choice)
+
+            {
+                
+            }
+        }
+    }
+}
